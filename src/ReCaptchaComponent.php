517082f9256635b30143751e21757ec1ac6d815e
Gayrat Usmonov <30826165+rabdavinci@users.noreply.github.com>
2023-04-19 03:58:24 +0500

    Create ReCaptchaComponent.php

---
+++ src/ReCaptchaComponent.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace kekaadrenalin\recaptcha3;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\di\Instance;
+use yii\helpers\Html;
+use yii\web\View;
+use yii\base\Widget;
+
+/**
+ * Class ReCaptchaComponent
+ * @package kekaadrenalin\recaptcha3
+ */
+class ReCaptchaComponent extends Widget
+{
+  /**
+   * Recaptcha component
+   * @var string|array|ReCaptcha
+   */
+  public $component = 'reCaptcha3';
+
+  /**
+   * @var string
+   */
+  public $buttonText = 'Submit';
+
+  /**
+   * @var string
+   */
+  public $actionName = 'homepage';
+
+  /**
+   * @var RecaptchaV3
+   */
+  private $_component = null;
+
+  /**
+   * @inheritdoc
+   * @throws InvalidConfigException
+   */
+  public function init()
+  {
+    parent::init();
+    $component = Instance::ensure($this->component, ReCaptcha::class);
+    if ($component == null) {
+      throw new InvalidConfigException('component is required.');
+    }
+    $this->_component = $component;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function run()
+  {
+    $this->_component->registerScript($this->getView());
+  }
+}
